73918f29337afd7003868b85af901621166f7bfc
Joe6276 <joendambuki16@gmail.com>
2021-12-31 15:00:02 +0300

    added a footer and Section

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -38,70 +38,101 @@ export default class Home extends Component {
         </div>
       </div>
 
- <footer className='text-center text-white' style={{ marginTop: '20px' ,backgroundColor: '#f1f1f1'}}>
+<p style={{fontStyle:'bold', marginLeft:'200px', marginTop:'100px',color:'black', fontFamily:'ubuntu', fontSize:'40px'}}> 
+Top Management </p>
 
-  <div className="container pt-4">
+
+<div style={{ backgroundColor:'#ffffff' ,opacity:1  ,marginTop:'0px'}} className='d-flex'>
+<div style={{marginLeft:'0px', height:'400px'}}>
+        <img  style={{marginLeft:'0px',  margin: '10px' ,width:'600px',  borderRadius:'10px' ,height:'500px'}} 
+        src ="https://cdn.pixabay.com/photo/2019/12/06/16/03/business-4677639__340.png"/>
+        </div>
+
+
+  <div>
+    <div style ={{ fontFamily:'ubuntu',  color:'black' ,width:'600px',  marginLeft:'30px', borderRadius:'10px' }} className='d-flex'>
+    <div style={{marginLeft:'0px'}}>
+        <img  style={{marginLeft:'0px',width:'150px',  borderRadius:'10px' ,height:'150px', marginTop:'10px'}} 
+        src ="https://thejitu.com/wp-content/uploads/2021/10/Samuel-Kirigha.jpg"/>
+        </div>
+        <div  style={{margin:'0px'}}>  
+          <h1 style={{marginLeft:'10px'}}>Dorcis Kirigha</h1>
+          <p style={{marginLeft:'10px'}}>Founder and Director of SendIT </p>
+          <p style={{marginLeft:'10px'}}>Tel :+254707256013</p>
+          <p style={{marginLeft:'10px'}}>dkirigha18@gmail.com</p>
+         
+</div>
  
-    <section className="mb-4">
-    
-      <a
-        className="btn btn-link btn-floating btn-lg text-dark m-1"
-        href="#!"
-        role="button"
-        data-mdb-ripple-color="dark"
-        ><i class="fab fa-facebook-f"></i
-      ></a>
+    </div>
 
-  
-      <a
-        className="btn btn-link btn-floating btn-lg text-dark m-1"
-        href="#!"
-        role="button"
-        data-mdb-ripple-color="dark"
-        ><i class="fab fa-twitter"></i
-      ></a>
 
-    
-      <a
-        className="btn btn-link btn-floating btn-lg text-dark m-1"
-        href="#!"
-        role="button"
-        data-mdb-ripple-color="dark"
-        ><i class="fab fa-google"></i
-      ></a>
+    {/* next */}
 
-      
-      <a
-        className="btn btn-link btn-floating btn-lg text-dark m-1"
-        href="#!"
-        role="button"
-        data-mdb-ripple-color="dark"
-        ><i class="fab fa-instagram"></i
-      ></a>
+    <div style ={{ fontFamily:'ubuntu' , color:'black',width:'600px',  marginLeft:'30px', borderRadius:'10px' }} className='d-flex'>
+    <div style={{marginLeft:'0px'}}>
+        <img  style={{marginLeft:'0px',width:'150px',  borderRadius:'10px' ,height:'150px', marginTop:'10px'}}  
+        src ="https://thejitu.com/wp-content/uploads/2021/10/Jonathan-Ndombuki.jpg"/>
+        </div>
+        <div>  
+          <h1 style={{marginLeft:'10px' ,fontStyle:'bold',}} >Jonathan Ndambuki </h1>
+          <p style={{marginLeft:'10px'}} >Co-Founder and CEO of SendIT </p>
+          <p style={{marginLeft:'10px'}} >Tel :+254712584549</p>
+          <p style={{marginLeft:'10px'}} >joendambuki16@gmail.com</p>
+         
+</div>
+ 
+    </div>
 
-    
-      <a
-        className="btn btn-link btn-floating btn-lg text-dark m-1"
-        href="#!"
-        role="button"
-        data-mdb-ripple-color="dark"
-        ><i class="fab fa-linkedin"></i
-      ></a>
-     
-      <a
-        className="btn btn-link btn-floating btn-lg text-dark m-1"
-        href="#!"
-        role="button"
-        data-mdb-ripple-color="dark"
-        ><i class="fab fa-github"></i
-      ></a>
-    
-</section>
+{/* next */}
 
+<div style ={{ fontFamily:'ubuntu' , color:'black',width:'600px',  marginLeft:'30px', borderRadius:'10px' }} className='d-flex'>
+    <div style={{marginLeft:'0px'}}>
+        <img  style={{marginLeft:'0px',width:'150px',  borderRadius:'10px' ,height:'150px', marginTop:'10px'}}  
+        src ="https://thejitu.com/wp-content/uploads/2021/09/Martin.jpg"/>
+        </div>
+        <div>  
+          <h1 style={{marginLeft:'10px' ,fontStyle:'bold',}} > Martin Mwangi </h1>
+          <p style={{marginLeft:'10px'}} >CHief Technical Officer of SendIT </p>
+          <p style={{marginLeft:'10px'}} >Tel :+254700207054</p>
+          <p style={{marginLeft:'10px'}} >martin.mwangi@gthejitu.com</p>
+         
+</div>
+ 
+    </div>
+
+  </div>
 
 </div>
-</footer>
 
+
+
+{/* Footer */}
+<section className="">
+  <footer className="text-center text-white" style={{backgroundColor: '#0a4275'}}>
+
+    <div className="container p-4 pb-0">
+      
+      <section className="">
+        <p className="d-flex justify-content-center align-items-center">
+          <span className="me-3">SendIT Logistics </span>
+          <button type="button" className="btn btn-outline-light btn-rounded">
+          Welcome All!
+          </button>
+        </p>
+      </section>
+      
+    </div>
+    
+
+    
+    <div className="text-center p-3" style={{backgroundColor:'black', opacity:0.2}}>
+      © 2022 Copyright:
+      <a className="text-white" href="https://mdbootstrap.com/">SendIT</a>
+    </div>
+   
+  </footer>
+ 
+</section>
 
 </div>
         )
